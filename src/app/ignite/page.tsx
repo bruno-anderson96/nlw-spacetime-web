d19d59e69e57cb7e66b0cc70c49e92bbb50d81f5
Caveira <brunoanderson.macedorosendo@gmail.com>
2023-05-20 12:16:08 -0300

    first commit

---
+++ src/app/ignite/page.tsx
@@ -0,0 +1,3 @@
+export default function Ignite() {
+  return <div className="h-screen bg-zinc-800"> Page Ignite </div>
+}
---
+++ src/depoimentos/page.tsx
@@ -0,0 +1,3 @@
+export default function Ignite(){
+  return <h1> Depoimentos </h1>
+}
